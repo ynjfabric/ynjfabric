8512904dd00db506490f76aaca38e21e716fe6b4
ynjfabric <ynjfabric@gmail.com>
2026-05-12 22:47:58 +0900

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,32 @@
+import { initializeApp }
+from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
+
+import { getFirestore }
+from "https://www.gstatic.com/firebasejs/12.13.0/firebase-firestore.js";
+
+import { getStorage }
+from "https://www.gstatic.com/firebasejs/12.13.0/firebase-storage.js";
+
+const firebaseConfig = {
+
+  apiKey: "네 API키",
+
+  authDomain: "ynjfabrickor.firebaseapp.com",
+
+  projectId: "ynjfabrickor",
+
+  storageBucket: "ynjfabrickor.firebasestorage.app",
+
+  messagingSenderId: "807605740695",
+
+  appId: "1:807605740695:web:4aaa88cf3387266bb635d2"
+};
+
+export const app =
+  initializeApp(firebaseConfig);
+
+export const db =
+  getFirestore(app);
+
+export const storage =
+  getStorage(app);
